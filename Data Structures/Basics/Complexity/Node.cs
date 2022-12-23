a17c98deda369f1ff210c275e90e79c563fce50d
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-23 19:18:00 +0200

    Add files via upload

---
+++ Data Structures/Basics/Complexity/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Trees
+{
+    internal class Node<T>
+    {
+        public Node(T value, params Node<T>[] items)
+        {
+            Value = value;
+            Children = items.ToList() ;
+        }
+
+        public T Value { get; set; }
+        public List<Node<T>> Children { get; set; }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
---
+++ Data Structures/Basics/Complexity/Tree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Trees
+{
+    internal class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public List<Node<T>> BFS(Node<T> root)
+        {
+            List<Node<T>> list = new List<Node<T>>();
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node<T> node = queue.Dequeue();
+                list.Add(node);
+                foreach (var child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return list;
+        }
+    }
+}
