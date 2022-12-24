9a8949b07584c2b960a94108aac6065f050b7952
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-24 17:19:42 +0200

    Add files via upload

--- Node.cs
+++ Node.cs
@@ -1,15 +1,10 @@
-﻿namespace Problem01.FasterQueue
+﻿namespace Problem02.DoublyLinkedList
 {
     public class Node<T>
     {
-        public Node(T item,Node<T> next)
-        {
-            Item = item;
-            Next = next;
-        }
-
         public T Item { get; set; }
 
         public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
     }
 }
