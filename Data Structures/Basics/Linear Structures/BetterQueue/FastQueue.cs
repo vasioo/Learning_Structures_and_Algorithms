ed5e4e62c6cb78e990291c6d6512db559e185e16
Vasil <vaskoorakov17@gmail.com>
2022-12-24 17:21:00 +0200

    added new queue and linked list exercises

---
+++ Data Structures/Basics/Linear Structures/BetterQueue/FastQueue.cs
@@ -0,0 +1,88 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var current = this.head;
+
+            while (current != null)
+            {
+                if (current.Item.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public T Dequeue()
+        {
+            this.EnsureNotEmpty();
+
+            var headItem =head.Item;
+            var newHead = head.Next;
+            head.Next = null;
+            head = newHead;
+
+            this.Count--;
+
+            return headItem;
+        }
+
+        public void Enqueue(T item)
+            //this implementation ensures O(1)
+            //because we always keep the pointer to the last element
+            //whenever there is a need for adding we have the position and
+            //just have to surpass the current element
+        {
+            var newNode = new Node<T>(item, null);
+            if (Count == 0)
+            {
+                head = tail = newNode;
+                this.Count++;
+                return;
+            }
+            tail.Next = newNode;
+            tail = tail.Next;
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.head;
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException();
+        }
+    }
+}
---
+++ Data Structures/Basics/Linear Structures/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,124 @@
+﻿namespace Problem02.DoublyLinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class DoublyLinkedList<T> : IAbstractLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T item)
+        {
+            var newNode = new Node<T>
+            {
+                Item = item,
+                Next = this.head
+            };
+            this.head = newNode;
+            this.Count++;
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node<T>
+            {
+                Item = item
+            };
+
+            if (this.head is null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                var current = this.head;
+
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                if (current.Next == null)
+                {
+                    this.tail = current;
+                }
+
+                current.Next = newNode;
+            }
+            newNode.Previous = tail;
+            this.Count++;
+        }
+
+        public T GetFirst()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException("The Linked list is empty");
+            }
+            else
+            {
+                return this.head.Item;
+            }
+        }
+
+        public T GetLast()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException("The Linked list is empty");
+            }
+            else
+            {
+                return this.tail.Item;
+            }
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException("The Linked list is empty");
+            }
+            else
+            {
+                this.head.Next = head;
+                this.head = null;
+                this.Count--;
+                return this.head.Item;
+            }
+
+        }
+
+        public T RemoveLast()
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException("The Linked list is empty");
+            }
+            else
+            {
+                this.tail.Previous = tail;
+                this.tail = null;
+                this.Count--;
+                return this.tail.Item;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this.head;
+
+            while (current != null)
+            {
+                yield return current.Item;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
