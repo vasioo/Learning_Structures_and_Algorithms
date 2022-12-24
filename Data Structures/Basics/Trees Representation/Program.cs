ea9060459b2dc325c7c58d7bfb0d2985a7c34684
Vasil <vaskoorakov17@gmail.com>
2022-12-24 12:02:21 +0200

    trees exercises

---
+++ Data Structures/Basics/Trees Representation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Implementing_Trees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = 
+                new Node<int>(7,
+                    new Node<int>(19,
+                        new Node<int>(1),
+                        new Node<int>(12),
+                        new Node<int>(31)),
+                    new Node<int>(21),
+                    new Node<int>(14,
+                        new Node<int>(23),
+                        new Node<int>(6)));
+
+            Tree<int> tree = new Tree<int>();
+            tree.Root = root;
+
+            List<Node<int>> treeToList = tree.BFS(root);
+
+            tree.DFS(root, 0);
+        }
+    }
+}
