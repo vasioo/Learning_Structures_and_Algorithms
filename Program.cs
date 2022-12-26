43b31777ae1a6440e9ded833d35d9c68d6053676
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-26 13:01:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Heap
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Traversing_BFS_and_DFS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfItems = int.Parse(Console.ReadLine());
+
+            int root=0;
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                if (i==0)
+                {
+                    root=input[0];
+                }
+            }
+            Tree<int> tree = new Tree<int>();
+            tree.Print();
         }
     }
 }
---
+++ Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversing_BFS_and_DFS
+{
+    internal class Tree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        public void Print(Node<T> node,int level)
+        {
+            Console.Write(new string(' ', level));
+            Console.WriteLine(node);
+            foreach (var child in node.Children)
+            {
+                Print(child, level + 2);
+            }
+        }
+    }
+}
