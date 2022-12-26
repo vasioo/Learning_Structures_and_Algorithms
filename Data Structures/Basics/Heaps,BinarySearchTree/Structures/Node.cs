39bf296771d357aba182bc5d84024b03a73c7d80
Vasil <vaskoorakov17@gmail.com>
2022-12-26 13:02:11 +0200

    added new implementations of heaps and binary search trees

---
+++ Data Structures/Basics/Heaps,BinarySearchTree/Structures/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Node<T>
+    {
+        public Node(T value, Node<T> left=null, Node<T> right=null)
+        {
+            Value = value;
+            LeftChild = left;
+            RightChild = right;
+        }
+
+        public T Value { get; set; }
+
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+    }
+}
---
+++ Data Structures/Basics/Heaps,BinarySearchTree/Structures/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> root = new Node<int>(1,
+                    new Node<int>(5,
+                         new Node<int>(2),
+                         new Node<int>(3)),
+                    new Node<int>(7,
+                         new Node<int>(9),
+                         new Node<int>(11))
+                );
+            BinaryTree<int> binaryTree = new BinaryTree<int>(root);
+
+            //pre order starts at root
+            Console.WriteLine(binaryTree.DFS_PreOrder(binaryTree.Root,0));
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+
+            //in order starts from the elements at the bottom of the tree
+            Console.WriteLine(binaryTree.DFS_InOrder(binaryTree.Root,0));
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("----------------------");
+
+            //post order makes the root last
+            Console.WriteLine(binaryTree.DFS_PostOrder(binaryTree.Root,0));
+        }
+    }
+}
