07619a9692b7f0e4119489fd8d99468634b4f467
Vasil <vaskoorakov17@gmail.com>
2022-12-28 15:16:17 +0200

    added new exercise

---
+++ Data Structures/Basics/Heaps,BinarySearchTree/Exercises/Cookies/CookiesProblem.cs
@@ -0,0 +1,26 @@
+﻿using Magnum.Collections;
+using System;
+
+namespace _04.CookiesProblem
+{
+    public class CookiesProblem
+    {
+        public int Solve(int k, int[] cookies)
+        {
+            var bag = new OrderedBag<int>(cookies);
+            var smallestElement = bag.GetFirst();
+            var operations = 0;
+
+            while (smallestElement < k && bag.Count > 1)
+            {
+                var smallestCookie = bag.RemoveFirst();
+                var secondSmallestCookie = bag.RemoveFirst();
+                operations++;
+
+                bag.Add(smallestCookie + 2 * secondSmallestCookie);
+                smallestElement = bag.GetFirst();
+            }
+            return smallestElement >= k ? operations : -1;
+        }
+    }
+}
