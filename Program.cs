eaf7d0aa3edc622d59d2fe9dd92bc3c369fbe4bc
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-29 17:49:40 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,90 @@
-﻿using System;
-
-namespace NestedLoops
+﻿namespace ConnectedAreasInMatrix
 {
     internal class Program
     {
-        private static int[] elements;
+        private static char[,] matrix;
+        private static int size = 0;
+
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
 
-            elements = new int[n];
+            matrix = new char[rows, cols];
 
-            Permutations(0);
+            for (int i = 0; i < rows; i++)
+            {
+                string elements = Console.ReadLine();
+
+                for (int c = 0; c < cols; c++)
+                {
+                    matrix[i, c] = elements[c];
+                }
+            }
+            var areas = new List<Area>();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    size = 0;
+                    ConnectedAreasInMatrix(r, c);
+                    if (size != 0)
+                    {
+                        areas.Add(new Area
+                        {
+                            Row = r,
+                            Col = c,
+                            Size = size
+                        });
+                    }
+                }
+            }
+
+            var sorted 
+                = areas.OrderByDescending(a => a.Size)
+                .ThenBy(a => a.Row)
+                .ThenBy(a => a.Col)
+                .ToList();
+
+            Console.WriteLine($"Total areas found: {areas.Count}");
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                var area = sorted[i];
+                Console.WriteLine($"Area #{i+1} at ({area.Row}, {area.Col}), size: {area.Size}");
+            }
         }
 
-        private static void Permutations(int index)
+        //this method explores all of the matrix
+        private static void ConnectedAreasInMatrix(int row, int col)
         {
-            if (index>=elements.Length)
+            if (IsOutsideOfMatrix(row, col) || IsWall(row, col) || IsVisited(row, col))
             {
-                Console.WriteLine(string.Join(" ",elements));
                 return;
             }
 
-            for (int i = 1; i <=elements.Length; i++)
-            {
-                elements[index]= i;
-                Permutations(index+1);
-            }
+            size += 1;
+            matrix[row, col] = 'v';
+
+            //using backtracking to find all the elements that do not touch any walls using recursion
+            ConnectedAreasInMatrix(row - 1, col);//Up
+            ConnectedAreasInMatrix(row + 1, col);//Down
+            ConnectedAreasInMatrix(row, col - 1);//Left
+            ConnectedAreasInMatrix(row, col + 1);//Right
+        }
+
+        private static bool IsVisited(int row, int col)
+        {
+            return matrix[row, col] == 'v';
+        }
+
+        private static bool IsWall(int row, int col)
+        {
+            return matrix[row, col] == '*';
+        }
+
+        private static bool IsOutsideOfMatrix(int row, int col)
+        {
+            return row < 0 || col < 0 || row >= matrix.GetLength(0) || col >= matrix.GetLength(1);
         }
     }
 }
