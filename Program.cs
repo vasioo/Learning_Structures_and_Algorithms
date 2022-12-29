98e5762fe9a9cc9d98c37626ad229fe553ce938e
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-29 13:14:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Permutations
+{
+    internal class Program
+    {
+        private static string[] array;
+        private static HashSet<string> permutations;
+        private static bool[] isUsed;
+        static void Main(string[] args)
+        {
+            array = Console.ReadLine().Split().ToArray();
+            Console.WriteLine("---------------");
+            permutations = new HashSet<string>();
+            //isUsed = new bool[array.Length];
+
+            PermuteWithoutRepetition(0);
+
+
+        }
+
+        //private static void PermuteWithRepetition(int index)
+        //{
+        //    if (index >= permutations.Length)
+        //    {
+        //        Console.WriteLine(string.Join(" ", permutations));
+        //        return;
+        //    }
+
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        if (!isUsed[i])
+        //        {
+        //            isUsed[i] = true;
+        //            permutations[index] = array[i];
+
+        //            PermuteWithRepetition(index + 1);
+        //            isUsed[i] = false;
+        //        }
+        //    }
+
+        private static void PermuteWithoutRepetition(int index)
+        {
+            if (index>=array.Length)
+            {
+                Console.WriteLine(string.Join(" ",array));
+                return;
+            }
+            PermuteWithoutRepetition(index + 1);
+            
+            var used= new HashSet<string> { array[index] };
+
+            for (int i = index+1; i < array.Length; i++)
+            {
+                if (!used.Contains(array[i]))
+                {
+                    Swap(index, i);
+                    PermuteWithoutRepetition(index + 1);
+                    Swap(index, i);
+
+                    used.Add(array[i]);
+                }
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            var temp = array[index];
+            array[index] = array[i];
+            array[i] = temp;
+        }
+    }
+}
