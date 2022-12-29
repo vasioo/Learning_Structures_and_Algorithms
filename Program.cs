cfbfb628be2a69aefb035a3cf3f584ee78745680
Vasil Orakov <vaskoorakov17@gmail.com>
2022-12-29 13:13:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,73 @@
-﻿namespace Labyrinth
+﻿namespace EightQueensProblem
 {
     internal class Program
     {
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+        private static HashSet<int> attackedRightDiagonals = new HashSet<int>();
         static void Main(string[] args)
         {
-            int rows = int.Parse(Console.ReadLine());
-            int cols = int.Parse(Console.ReadLine());
+            var board = new bool[8, 8];
 
-            var lab = new char[rows, cols];
-
-            for (int r = 0; r < rows; r++)
-            {
-                var colElements = Console.ReadLine();
-                for (int c = 0; c < cols; c++)
-                {
-                    lab[r, c] = colElements[c];
-                }
-            }
-            FindPaths(lab, 0, 0, new List<string>(), string.Empty);
+            PlaceQueens(board, 0);
         }
 
-        private static void FindPaths(char[,] lab, int row, int col, List<string> directions, string direction)
+        private static void PlaceQueens(bool[,] board, int row)
         {
-            //validation
-            if (row < 0 || row >= lab.GetLength(0) || col < 0 || col >= lab.GetLength(1))
+            if (row >= board.GetLength(0))
             {
+                PrintBoard(board);
                 return;
             }
-            //if this symbol is seen the process stops or the v is seen it has been visited
-            if (lab[row, col] == '*' || lab[row,col]=='v')
+            for (int col = 0; col < board.GetLength(1); col++)
             {
-                return;
-            }
-            //if reaching the end we remove the combination 
-            if (lab[row, col] == 'e')
-            {
-                Console.WriteLine(string.Join(' ', directions));
-                directions.RemoveAt(directions.Count - 1);
-                return;
-            }
-            lab[row, col] = 'v';
-            directions.Add(direction);
-            FindPaths(lab, row - 1, col, directions, "U");//up
-            FindPaths(lab, row + 1, col, directions, "D");//down
-            FindPaths(lab, row, col - 1, directions, "L");//left
-            FindPaths(lab, row, col + 1, directions, "R");//right
+                //If a queen can be placed
+                if (CanBePlaced(row, col))
+                {
+                    attackedRows.Add(row);
+                    attackedCols.Add(col);
+                    attackedLeftDiagonals.Add(row - col);
+                    attackedRightDiagonals.Add(row + col);
+                    board[row, col] = true;
 
-            lab[row, col] = '-';
-            directions.RemoveAt(directions.Count - 1);
+                    PlaceQueens(board, row + 1);
+
+                    attackedRows.Remove(row);
+                    attackedCols.Remove(col);
+                    attackedLeftDiagonals.Remove(row - col);
+                    attackedRightDiagonals.Remove(row + col);
+                    board[row, col] = false;
+                }
+            }
+        }
+
+        private static void PrintBoard(bool[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (board[row, col])
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("- ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool CanBePlaced(int row, int col)
+        {
+            return !attackedRows.Contains(row) &&
+            !attackedCols.Contains(col) &&
+            !attackedLeftDiagonals.Contains(row - col) &&
+            !attackedRightDiagonals.Contains(row + col);
         }
     }
 }
