0c3694c7898cda0f4fd705be36669e16d022f09f
Vasil <vaskoorakov17@gmail.com>
2022-12-29 13:16:14 +0200

    added combinatorical problems

---
+++ Algorithms/Basics/Combinatorial Problems/Combinations/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Combinations
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] combinations;
+        private static bool[] isUsed;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            combinations = new string[k];
+            isUsed = new bool[elements.Length];
+
+            Combinations(0,0);
+        }
+
+        private static void Combinations(int index,int elementsStartIndex)
+        {
+            if (index >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = elementsStartIndex; i < elements.Length; i++)
+            {
+                if (!isUsed[i])
+                {
+                    //isUsed[i] = true;
+                    combinations[index] = elements[i];
+                    Combinations(index + 1,i+1);
+                    //isUsed[i] = false;
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Basics/Combinatorial Problems/Variations/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Variations
+{
+    internal class Program
+    {
+        private static int k;
+        private static string[] elements;
+        private static string[] variations;
+        private static bool[] isUsed;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+
+            variations = new string[k];
+            isUsed = new bool[elements.Length];
+
+            Variations(0);
+        }
+
+        private static void Variations(int index)
+        {
+            if (index>= variations.Length)
+            {
+                Console.WriteLine(string.Join(" ",variations));
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (!isUsed[i])
+                {
+                    //isUsed[i] = true;
+                    variations[index] = elements[i];
+                    Variations(index+1);
+                    //isUsed[i] = false;
+                }
+            }
+        }
+    }
+}
