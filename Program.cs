101b88a3cb5d108e2226765eb7e5d3f3583b887c
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-02 18:05:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,63 @@
-﻿namespace DFS
+﻿namespace BFS
 {
     internal class Program
     {
-        private static Dictionary<int, List<int>> graph;
-        private static HashSet<int> visited;
-
+        private static List<int>[] graph;
+        private static bool[] visited;
         static void Main(string[] args)
         {
-            graph = new Dictionary<int, List<int>>
-            {
-                {1, new List<int> {19, 21, 14}},
-                {19, new List<int> {7, 12, 31, 21}},
-                {7, new List<int> {1}},
-                {31, new List<int> {21}},
-                {21, new List<int> {14}},
-                {23, new List<int> {21}},
-                {14, new List<int> {23, 6}},
-                {12, new List<int> ()},
-                {6, new List<int> ()}
-            };
+            int n = int.Parse(Console.ReadLine());
 
-            visited = new HashSet<int>();
+            graph = new List<int>[n];
+            visited = new bool[n];
 
-            foreach (var node in graph.Keys)
+            for (int node = 0; node < n; node++)
             {
-                DFS(node);
+                var line = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    graph[node] = new List<int>();
+                }
+                else
+                {
+                    var children = line.Split().Select(int.Parse).ToList();
+                    graph[node] = children;
+                }
+            }
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+                var component = new List<int>();
+                BFS(node, component);
+
+                Console.WriteLine($"Connected components {string.Join(" ", component)}");
             }
         }
 
-        private static void DFS(int node)
+        private static void BFS(int startNode, List<int> component)
         {
-            if (visited.Contains(node))
-            {
-                return;
-            }
+            var queue = new Queue<int>();
+            queue.Enqueue(startNode);
 
-            visited.Add(node);
+            visited[startNode] = true;
 
-            foreach (var child in graph[node])
+            while (queue.Count > 0)
             {
-                DFS(child);
+                var node = queue.Dequeue();
+                component.Add(node);
+                foreach (var child in graph[node])
+                {
+                    if (!visited[child])
+                    {
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
+                }
             }
-            Console.WriteLine(node) ;
         }
     }
 }
