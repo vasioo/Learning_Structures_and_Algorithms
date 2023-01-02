5271ab3b44ee13a9830120beb0214befc7a6c4f2
Vasil <vaskoorakov17@gmail.com>
2023-01-02 14:44:14 +0200

    added new sorting and searching algos

---
+++ Algorithms/Basics/Searching,Sorting/Sorting/QuickSort/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            QuickSort(numbers, 0, numbers.Length - 1);
+            Console.WriteLine(string.Join("", numbers));
+        }
+
+        private static void QuickSort(int[] numbers, int start, int end)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            var pivot = start;
+            var leftPointer = start+1;
+            var rightPointer = end;
+
+            while (leftPointer<=rightPointer)
+            {
+                if (numbers[leftPointer] > numbers[pivot] 
+                    && numbers[rightPointer] < numbers[pivot])
+                {
+                    Swap(numbers,leftPointer,rightPointer);
+                }
+                if (numbers[leftPointer] <= numbers[pivot])
+                {
+                    leftPointer += 1;
+                }
+                if (numbers[rightPointer] >= numbers[pivot])
+                {
+                    rightPointer -= 1;
+                }
+            }
+            Swap(numbers,pivot, rightPointer);
+
+            QuickSort(numbers, start,rightPointer-1);
+            QuickSort(numbers, rightPointer + 1, end);
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
