33825bfa7bbb10bbc5d7bec4d8193fbcc9e2751e
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-02 18:03:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AdjencyLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new List<int>[]
+            {
+                new List<int>{3, 6},
+                new List<int>{2, 3, 4, 5, 6},
+                new List<int>{2, 4, 5},
+                new List<int>{0, 1, 5},
+                new List<int>{1, 2, 6},
+                new List<int>{1, 2, 3},
+                new List<int>{0, 1, 4}
+            };
+
+            Console.WriteLine(string.Join(" ", graph[0]));
+
+            //how to add indices to the graph elements
+            var mapping = new Dictionary<string, int>
+            {
+                {"Sofia",1 },
+                {"Varna",2 }
+            };
+             
+        }
+    }
+}
