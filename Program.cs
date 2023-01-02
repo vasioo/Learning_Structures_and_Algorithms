1b87fc401e2b0ca830eab9928aa680c889f4de88
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-02 20:48:09 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,84 +1,85 @@
-﻿namespace DistanceBetweenVerticles
+﻿namespace AreasInMatrix
 {
     internal class Program
     {
-        private static Dictionary<int, List<int>> mainGraph;
-        private static Dictionary<int, List<int>> connections;
+        private static char[,] matrix;
+        private static bool[,] visited;
+        private static Dictionary<char, int> areas;
 
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int p = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
 
-            mainGraph = new Dictionary<int, List<int>>();
-            connections = new Dictionary<int, List<int>>();
+            matrix = new char[n, m];
+            visited = new bool[n, m];
+            areas = new Dictionary<char, int>();
 
-            for (int i = 0; i < n; i++)
+            for (int row = 0; row < n; row++)
             {
-                var input = Console.ReadLine().Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string input = Console.ReadLine();
 
-                var parent = int.Parse(input[0]);
-                if (input.Length == 1)
+                List<char> elements = new List<char>(input.ToCharArray());
+
+                for (int col = 0; col < m; col++)
                 {
-                    mainGraph[parent] = new List<int>();
-                }
-                else
-                {
-                    var children = input[1].Split().Select(int.Parse).ToList();
-                    mainGraph[parent] = children;
+                    matrix[row, col] = elements[col];
                 }
             }
 
-            for (int i = 0; i < p; i++)
+            for (int row = 0; row < n; row++)
             {
-                int[] s = Console.ReadLine().Split("-").Select(int.Parse).ToArray();
-                int start = s[0];
-                int destination = s[1];
-
-                Console.WriteLine($"{start},{destination} -> {ShortestDistance(start, destination)}");
-            }
-        }
-        //BFS
-        private static int ShortestDistance(int start, int destination)
-        {
-            var queue = new Queue<int>();
-            queue.Enqueue(start);
-            var visited = new HashSet<int>();
-            var parent = new Dictionary<int, int> { { start, -1 } };
-            while (queue.Count > 0)
-            {
-                var node = queue.Dequeue();
-
-                if (node == destination)
+                for (int col = 0; col < m; col++)
                 {
-                    return Steps(parent,destination);
-                }
-
-                foreach (var child in mainGraph[node])
-                {
-                    if (visited.Contains(child))
+                    if (visited[row, col])
                     {
                         continue;
                     }
-                    visited.Add(child);
-                    queue.Enqueue(child);
-                    parent[child] = node;
+                    var parentNode = matrix[row, col];
+                    DFS(row, col, parentNode);
+
+                    if (areas.ContainsKey(parentNode))
+                    {
+                        areas[parentNode]++;
+                    }
+                    else
+                    {
+                        areas[parentNode] = 1;
+                    }
                 }
             }
-
-            return -1;
-        }
-
-        private static int Steps(Dictionary<int, int> parent, int destination)
-        {
-            var steps = -1;
-            var node = destination;
-            while (node!=-1)
+            int totalCounter = 0;
+            foreach (var area in areas)
             {
-                node = parent[node];
-                steps++;
+                totalCounter += area.Value;
             }
-            return steps;
+            Console.WriteLine($"Areas: {totalCounter}");
+            foreach (var area in areas)
+            {
+                Console.WriteLine($"Letter '{area.Key}' -> {area.Value}");
+            }
+        }
+        private static void DFS(int row, int col, char parentNode)
+        {
+            if (row < 0 || row >= matrix.GetLength(0) 
+                || col < 0 || col >= matrix.GetLength(1))
+            {
+                return;
+            }
+            if (visited[row, col])
+            {
+                return;
+            }
+            if (matrix[row, col] != parentNode)
+            {
+                return;
+            }
+
+            visited[row,col] = true;
+            DFS(row, col - 1, parentNode);
+            DFS(row, col + 1, parentNode);
+            DFS(row - 1, col, parentNode);
+            DFS(row + 1, col, parentNode);
         }
     }
 }
