8dfdebd06df0c80b8cded1b5cceb4a27cf40b2fb
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-02 14:43:15 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace InsertionSort
+﻿namespace QuickSort
 {
     internal class Program
     {
@@ -6,24 +6,42 @@
         {
             var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            InsertionSort(numbers);
-
-            Console.WriteLine(string.Join(" ", numbers));
+            QuickSort(numbers, 0, numbers.Length - 1);
+            Console.WriteLine(string.Join("", numbers));
         }
 
-        private static void InsertionSort(int[] numbers)
+        private static void QuickSort(int[] numbers, int start, int end)
         {
-            for (int i = 1; i < numbers.Length; i++)
+            if (start >= end)
             {
-                var j = i;
+                return;
+            }
+            var pivot = start;
+            var leftPointer = start+1;
+            var rightPointer = end;
 
-                while (j > 0 && numbers[j - 1] > numbers[j])
+            while (leftPointer<=rightPointer)
+            {
+                if (numbers[leftPointer] > numbers[pivot] 
+                    && numbers[rightPointer] < numbers[pivot])
                 {
-                    Swap(numbers, j, j - 1);
-                    j--;
+                    Swap(numbers,leftPointer,rightPointer);
+                }
+                if (numbers[leftPointer] <= numbers[pivot])
+                {
+                    leftPointer += 1;
+                }
+                if (numbers[rightPointer] >= numbers[pivot])
+                {
+                    rightPointer -= 1;
                 }
             }
+            Swap(numbers,pivot, rightPointer);
+
+            QuickSort(numbers, start,rightPointer-1);
+            QuickSort(numbers, rightPointer + 1, end);
         }
+
         private static void Swap(int[] numbers, int first, int second)
         {
             var temp = numbers[first];
