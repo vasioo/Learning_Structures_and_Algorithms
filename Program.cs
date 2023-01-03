e4439582a1ad3559069bb354fbde85a46e828501
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-03 20:40:44 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,30 @@
-﻿namespace WordDifferences
+﻿namespace ConnectingCables
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string s1 = Console.ReadLine();
-            string s2 = Console.ReadLine();
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            var dp = new int[s1.Length + 1, s2.Length + 1];
+            var positions = Enumerable.Range(1, numbers.Length).ToArray();
+            //they are the same
+            var dp = new int[numbers.Length + 1, positions.Length + 1];
 
-            for (int row = 1; row < dp.GetLength(0); row++)
-            {
-                dp[row, 0] = row;
-            } 
-            for (int col = 1; col < dp.GetLength(1); col++)
-            {
-                dp[0, col] = col;
-            }
             for (int row = 1; row < dp.GetLength(0); row++)
             {
                 for (int col = 1; col < dp.GetLength(1); col++)
                 {
-                    if (s1[row - 1] == s2[col-1])
+                    if (numbers[row - 1] == positions[col - 1])
                     {
-                        dp[row, col] = dp[row - 1, col-1];
+                        dp[row, col] = dp[row, col - 1]+1;
                     }
                     else
                     {
-                        dp[row, col] = Math.Min(dp[row - 1, col], dp[row,col-1])+1;
+                        dp[row, col] = Math.Max(dp[row - 1, col], dp[row, col - 1]);
                     }
                 }
             }
-            Console.WriteLine("Deletions and Insertions: "+dp[s1.Length,s2.Length]);
+            Console.WriteLine("Maximum pairs connected: "+dp[numbers.Length,numbers.Length]);
         }
     }
 }
