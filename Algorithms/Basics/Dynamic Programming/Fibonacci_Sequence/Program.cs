5068813e3c3e1ed0e612e22374423900f3fef052
Vasil <vaskoorakov17@gmail.com>
2023-01-03 18:04:53 +0200

    solved new exercises

---
+++ Algorithms/Basics/Dynamic Programming/Fibonacci_Sequence/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Dynamic_Optimizitaion
+{
+    internal class Program
+    {
+        private static Dictionary<int, long> cache = new Dictionary<int, long>();
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFibonacci(n));
+        }
+
+        //private static long CalcFibonacci(int n)
+        //{
+        //    //using recursion it uses a lot of memory and at 50th element it stops
+        //    if (n == 1)
+        //    {
+        //        return 0;
+        //    }
+        //    if (n == 2)
+        //    {
+        //        return 1;
+        //    }
+        //    return CalcFibonacci(n - 1) + CalcFibonacci(n - 2);
+        //}
+        private static long CalcFibonacci(int n)
+        {
+            if (cache.ContainsKey(n))
+            {
+                return cache[n];
+            }
+            if (n < 2)
+            {
+                return n;
+            }
+            var result = CalcFibonacci(n - 1) + CalcFibonacci(n - 2);
+            cache[n] = result;
+            return result;
+        }
+    }
+}
---
+++ Algorithms/Basics/Dynamic Programming/MoveIt/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Move_Down_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            var matrix = new int[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                var rowElements = Console.ReadLine()
+                     .Split()
+                     .Select(int.Parse)
+                     .ToArray();
+                for (int c = 0; c < rowElements.Length; c++)
+                {
+                    matrix[r, c] = rowElements[c];
+                }
+            }
+            var dynamicProgramming = new int[rows, cols];
+
+            dynamicProgramming[0, 0] = matrix[0, 0];
+
+            for (int c = 1; c < cols; c++)
+            {
+                dynamicProgramming[0, c] = dynamicProgramming[0, c - 1] + matrix[0, c];
+            }
+            for (int r = 1; r < rows; r++)
+            {
+                dynamicProgramming[r, 0] = dynamicProgramming[r - 1, 0] + matrix[r, 0];
+            }
+            for (int r = 1; r < rows; r++)
+            {
+                for (int c = 1; c < cols; c++)
+                {
+                    var upper = dynamicProgramming[r - 1, c];
+                    var left = dynamicProgramming[r, c - 1];
+
+                    dynamicProgramming[r, c] = Math.Max(left, upper) + matrix[r, c];
+                }
+            }
+            var path = new Stack<string>();
+            var row = rows - 1;
+            var col = cols - 1;
+
+            while (row > 0 && col > 0)
+            {
+                path.Push($"[{row},{col}]");
+                var upper = dynamicProgramming[row - 1, col];
+                var left = dynamicProgramming[row, col - 1];
+
+                if (upper > left)
+                {
+                    row--;
+                }
+                else
+                {
+                    col--;
+                }
+            }
+            while (row>0)
+            {
+                path.Push($"[{row},{col}]");
+                row--;
+            }
+            while (col>0)
+            {
+                path.Push($"[{row},{col}]");
+                col--;
+            }
+            path.Push($"[{row},{col}]");
+
+            Console.WriteLine(string.Join(" ",path));
+        }
+    }
+}
