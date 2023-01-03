60fe6bc17c1f680df8d8c215667db9a7638f2508
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-03 18:03:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace SubsetSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { 3, 5, 2 };
+            int targetSum = 54;
+
+            var sums = new bool[targetSum + 1];
+            sums[0] = true;
+
+            for (int sum = 0; sum < sums.Length; sum++)
+            {
+                if (!sums[sum])
+                {
+                    continue;
+                }
+                foreach (var num in nums)
+                {
+                    var newSum = sum + num;
+
+                    if (newSum > targetSum)
+                    {
+                        continue;
+                    }
+                    sums[newSum] = true;
+                }
+            }
+            var subset = new List<int>();
+            while (targetSum > 0)
+            {
+                foreach (var num in nums)
+                {
+                    var preSum = targetSum - num;
+                    if (preSum >= 0 && sums[preSum])
+                    {
+                        subset.Add(num);
+                        targetSum = preSum;
+                        if (targetSum==0)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ",subset));
+        }
+    }
+}
