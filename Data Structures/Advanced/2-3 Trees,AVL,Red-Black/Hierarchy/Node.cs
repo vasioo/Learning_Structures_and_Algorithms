b53b177025fc0bef9a2ff9afa042013a82dce32d
Vasil <vaskoorakov17@gmail.com>
2023-01-04 20:31:00 +0200

    solved tree problems

---
+++ Data Structures/Advanced/2-3 Trees,AVL,Red-Black/Hierarchy/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_2_3_AVL
+{
+    internal class Node<T>
+    {
+        public T Value { get; private set; }
+
+        public Node<T> Parent { get; set; }
+
+        public List<Node<T>> Children { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Children = new List<Node<T>>();
+        }
+
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+
+    }
+}
