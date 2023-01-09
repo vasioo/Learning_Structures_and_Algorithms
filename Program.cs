39580c35d51348a44cd140fc9408350d61ea4b37
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-09 20:57:49 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,108 +1,101 @@
-﻿using Wintellect.PowerCollections;
-
-namespace Prim
+﻿namespace BellmanFord
 {
     public class Edge
     {
-        public int First { get; set; }
+        public int From { get; set; }
 
-        public int Second { get; set; }
+        public int To { get; set; }
 
         public int Weight { get; set; }
     }
 
     internal class Program
     {
-        private static Dictionary<int, List<Edge>> edgesByNodes;
-        private static HashSet<int> forest;
+        private static List<Edge> edges;
 
         static void Main(string[] args)
         {
-            var e = int.Parse(Console.ReadLine());
+            int nodes = int.Parse(Console.ReadLine());
+            int edgesCount = int.Parse(Console.ReadLine());
 
-            edgesByNodes = ReadGraph(e);
+            edges = new List<Edge>();
 
-            foreach (var node in edgesByNodes.Keys)
+            for (int i = 0; i < edgesCount; i++)
             {
-                if (!forest.Contains(node))
-                {
-                    Prim(node);
-                }
-            }
-        }
+                var data = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 
-        private static void Prim(int node)
-        {
-            forest.Add(node);
-
-            var queue = new OrderedBag<Edge>(
-                edgesByNodes[node],
-                Comparer<Edge>.Create((f, s) => f.Weight - s.Weight));
-
-            while (queue.Count > 0)
-            {
-                var edge = queue.RemoveFirst();
-                var nonTreeNode = -1;
-
-                if (forest.Contains(edge.First) && !forest.Contains(edge.Second))
-                {
-                    nonTreeNode = edge.Second;
-                }
-
-                else if (forest.Contains(edge.Second) && !forest.Contains(edge.First))
-                {
-                    nonTreeNode = edge.First;
-                }
-
-                if (nonTreeNode == -1)
-                {
-                    continue;
-                }
-
-                Console.WriteLine($"{edge.First} - {edge.Second}");
-
-                forest.Add(nonTreeNode);
-                queue.AddMany(edgesByNodes[nonTreeNode]);
-            }
-        }
-
-        private static Dictionary<int, List<Edge>> ReadGraph(int e)
-        {
-            var result = new Dictionary<int, List<Edge>>();
-
-            for (int i = 0; i < e; i++)
-            {
-                var data = Console.ReadLine()
-                    .Split(", ")
-                    .Select(int.Parse)
-                    .ToArray();
-
-
-                var firstNode = data[0];
-                var secondNode = data[1];
+                var from = data[0];
+                var to = data[1];
                 var weight = data[2];
 
-                if (!result.ContainsKey(firstNode))
+                edges.Add(new Edge
                 {
-                    result.Add(firstNode, new List<Edge>());
-                }
-
-                if (!result.ContainsKey(secondNode))
-                {
-                    result.Add(secondNode, new List<Edge>());
-                }
-
-                var edge = new Edge
-                {
-                    First = firstNode,
-                    Second = secondNode,
+                    From = from,
+                    To = to,
                     Weight = weight
-                };
-
-                result[firstNode].Add(edge);
-                result[secondNode].Add(edge);
+                });
             }
-            return result;
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var distance = new double[nodes + 1];
+            Array.Fill(distance, double.PositiveInfinity);
+
+            distance[source] = 0;
+            var prev = new int[nodes + 1];
+            Array.Fill(prev, -1);
+
+            for (int i = 0; i < nodes - 1; i++)
+            {
+                var updated = false;
+                foreach (var edge in edges)
+                {
+                    if (double.IsPositiveInfinity(edge.From))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance < distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                        prev[edge.To] = edge.From;
+                        updated = true;
+                    }
+                }
+                if (!updated)
+                {
+                    break;
+                }
+                foreach (var edge in edges)
+                {
+                    if (double.IsPositiveInfinity(edge.From))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance < distance[edge.To])
+                    {
+                        Console.WriteLine("Negative Cycle Detected");
+                        return;
+                    }
+                }
+            }
+            var path = new Stack<int>();
+            var node = destination;
+
+            while (node != -1)
+            {
+                path.Push(node);
+                node = prev[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+            Console.WriteLine(distance[destination]);
         }
     }
 }
