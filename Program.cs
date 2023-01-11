27868172587043d396c3da2d3cd00966567ac9bb
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-11 23:02:59 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,98 +1,118 @@
-﻿namespace ElectricalSubstationNetwork
+﻿namespace MaximumTasksAssignment
 {
     internal class Program
     {
-        private static List<int>[] graph;
-        private static List<int>[] reversedGraph;
-        private static Stack<int> sorted;
+        private static int[,] graph;
+        private static int[] parents;
 
         static void Main(string[] args)
         {
-            int nodesCount = int.Parse(Console.ReadLine());
-            int linesCount = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+            int tasksCount = int.Parse(Console.ReadLine());
 
-            (graph, reversedGraph) = ReadGraph(nodesCount, linesCount);
+            graph = ReadGraph(peopleCount, tasksCount);
+            var nodes = graph.GetLength(0);
 
-            sorted = TopologicalSorting();
+            parents = new int[nodes];
 
-            var visited = new bool[nodesCount];
+            Array.Fill(parents, -1);
 
-            while (sorted.Count > 0)
+            var start = 0;
+            var target = nodes - 1;
+
+            while (BFS(start, target))
             {
-                var node = sorted.Pop();
+                var node = target;
 
-                if (visited[node])
+                while (node != start)
                 {
-                    continue;
+                    var parent = parents[node];
+
+                    graph[parent, node] = 0;
+                    graph[node, parent] = 1;
+
+                    node = parent;
                 }
-
-                var component = new Stack<int>();
-                DFS(reversedGraph, node, visited, component);
-
-                Console.WriteLine(string.Join(", ",component));
+            }
+            for (int person = 1; person <= peopleCount; person++)
+            {
+                for (int task = peopleCount + 1; task <= peopleCount + tasksCount; task++)
+                {
+                    if (graph[task, person] > 0)
+                    {
+                        Console.WriteLine($"{(char)(64 + person)} - {task - peopleCount}");
+                        break;
+                        
+                        //3
+                        //3
+                        //YNY
+                        //NYY
+                        //YYN
+                    }
+                }
             }
         }
 
-        private static Stack<int> TopologicalSorting()
+        private static int[,] ReadGraph(int people, int tasks)
         {
-            var stack = new Stack<int>();
-            var visited = new bool[graph.Length];
+            var nodes = people + tasks + 2;
+            var result = new int[nodes, nodes];
+            var start = 0;
+            var target = nodes - 1;
 
-            for (int node = 0; node < graph.Length; node++)
+            for (int person = 1; person <= people; person++)
             {
-                if (!visited[node])
+                result[start, person] = 1;
+            }
+            for (int task = people + 1; task <= people + tasks; task++)
+            {
+                result[task, target] = 1;
+            }
+
+            for (int person = 1; person <= people; person++)
+            {
+                var personTasks = Console.ReadLine();
+
+                for (int task = 0; task < personTasks.Length; task++)
                 {
-                    DFS(graph, node, visited, stack);
+                    if (personTasks[task] == 'Y')
+                    {
+                        result[person, people + 1 + task] = 1;
+                    }
                 }
             }
-            return stack;
+            return result;
+
         }
 
-        private static void DFS(List<int>[] targetGraph, int node, bool[] visited, Stack<int> stack)
+        private static bool BFS(int start, int target)
         {
-            if (visited[node])
+            var visited = new bool[graph.GetLength(0)];
+            var queue = new Queue<int>();
+
+            visited[start] = true;
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
             {
-                return;
-            }
+                var node = queue.Dequeue();
 
-            visited[node] = true;
-
-            foreach (var child in targetGraph[node])
-            {
-                DFS(targetGraph, child, visited, stack);
-            }
-        }
-
-        private static (List<int>[] original, List<int>[] reversed)
-            ReadGraph(int nodesCount, int linesCount)
-        {
-            var original = new List<int>[nodesCount];
-            var reversed = new List<int>[nodesCount];
-
-            for (int node = 0; node < original.Length; node++)
-            {
-                original[node] = new List<int>();
-                reversed[node] = new List<int>();
-            }
-
-            for (int i = 0; i < linesCount; i++)
-            {
-                var parts = Console.ReadLine()
-                    .Split(", ")
-                    .Select(int.Parse)
-                    .ToArray();
-
-                var node = parts[0];
-
-                for (int j = 1; j < parts.Length; j++)
+                if (node == target)
                 {
-                    var child = parts[j];
+                    return true;
+                }
 
-                    original[node].Add(child);
-                    reversed[child].Add(node);
+                for (int child = 0; child < graph.GetLength(1); child++)
+                {
+                    if (!visited[child] && graph[node, child] > 0)
+                    {
+                        parents[child] = node;
+                        visited[child] = true;
+                        queue.Enqueue(child);
+                    }
                 }
             }
-            return (original, reversed);
+            return false;
         }
     }
 }
