d31b896eeebd0b23d776db088ca9da0de5d60003
Vasil Orakov <vaskoorakov17@gmail.com>
2023-01-12 19:39:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,60 +1,102 @@
-﻿namespace Rod_Cutting
+﻿namespace Knapsack
 {
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Value { get; set; }
+    }
+
     internal class Program
     {
-        private static int[] bestPrices;
-        private static int[] combo;
 
         static void Main(string[] args)
         {
-            var prices = Console.ReadLine()
-                .Split()
-                .Select(int.Parse)
-                .ToArray();
+            int maxCapacity = int.Parse(Console.ReadLine());
 
-            var length = int.Parse(Console.ReadLine());
+            var items = ReadItems();
 
-            bestPrices = new int[length + 1];
-            combo = new int[length + 1];
+            var dp = new int[items.Count + 1, maxCapacity + 1];
+            var included = new bool[items.Count + 1, maxCapacity + 1];
 
-            Console.WriteLine(CutRod(length, prices));
-
-            while (length!=0)
+            for (int row = 1; row < dp.GetLength(0); row++)
             {
-                Console.Write($"{combo[length]} ");
-                length -= combo[length];
+                var currentItem = items[row - 1];
+                for (int capacity = 1; capacity < dp.GetLength(1); capacity++)
+                {
+                    var skipItem = dp[row - 1, capacity];
+
+                    if (currentItem.Weight > capacity)
+                    {
+                        dp[row, capacity] = skipItem;
+                        continue;
+                    }
+
+                    var takeItem = currentItem.Value + dp[row - 1, capacity - currentItem.Weight];
+
+                    if (takeItem > skipItem)
+                    {
+                        dp[row, capacity] = takeItem;
+                        included[row, capacity] = true;
+                    }
+                    else
+                    {
+                        dp[row, capacity] = skipItem;
+                    }
+                }
+            }
+            var includedItems = new SortedSet<string>();
+
+            var totalValue = dp[items.Count, maxCapacity];
+            var totalWeight = 0;
+
+            for (int row = included.GetLength(0) - 1; row >= 0; row--)
+            {
+                if (included[row, maxCapacity])
+                {
+                    var includedItem = items[row - 1];
+
+                    maxCapacity -= includedItem.Weight;
+                    totalWeight += includedItem.Weight;
+
+                    includedItems.Add(includedItem.Name);
+                }
+            }
+
+            Console.WriteLine($"Total Weight: {totalWeight}");
+            Console.WriteLine($"Total Value: {totalValue}");
+
+            foreach (var item in includedItems)
+            {
+                Console.WriteLine(item);
             }
         }
 
-        private static int CutRod(int length, int[] prices)
+        private static List<Item> ReadItems()
         {
-            if (length == 0)
+            var result = new List<Item>();
+
+            while (true)
             {
-                return 0;
-            }
+                var line = Console.ReadLine();
 
-            if (bestPrices[length] != 0)
-            {
-                return bestPrices[length];
-            }
-
-            var bestPrice = prices[length];
-            var bestCombo = length;
-
-            for (int i = 1; i <= length; i++)
-            {
-                var currentPrice = prices[i] + CutRod(length - i, prices);
-
-                if (currentPrice > bestPrice)
+                if (line == "end")
                 {
-                    bestPrice = currentPrice;
-                    bestCombo = i;
+                    break;
                 }
-            }
-            bestPrices[length] = bestPrice;
-            combo[length] = bestCombo;
 
-            return bestPrice;
+                var itemData = line.Split();
+
+                result.Add(new Item
+                {
+                    Name = itemData[0],
+                    Weight = int.Parse(itemData[1]),
+                    Value = int.Parse(itemData[2])
+                });
+            }
+            return result;
         }
     }
 }
